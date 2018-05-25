8994f7a62375520211e38b0e5cd3a325642ab27b
Mo Mesgin <mo.mesgin@gmail.com>
2018-05-24 23:01:21 -0700

    add statusscreen & maincontainer comps

---
+++ MainContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import StatusScreen from './StatusScreen'
+
+export default class MainContainer extends Component {
+  render() {
+    return (
+      <View style={styles.container} >
+        <StatusScreen />
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
